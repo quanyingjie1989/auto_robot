4199efada71ec8b14c2e840693b3aa12e0492da7
quanyingjie1989 <65935473+quanyingjie1989@users.noreply.github.com>
2020-06-16 22:32:23 +0800

    test
    test

--- pages/utils/facebook-global.js
+++ pages/utils/facebook-global.js
@@ -1,43 +1,43 @@
 // FaceBook 계정 
 global.facebook_Id_1 = "quan_yingjie@nhn-st.com";
-global.facebook_pW_1 = "quantaihao1-24";
+global.facebook_pW_1 = "xingjunnihao123";
 
 global.facebook_Id_2 = "quan_yingjie@nhn-st.com";
-global.facebook_pW_2 = "quantaihao1-24";
+global.facebook_pW_2 = "xingjunnihao123";
 
 global.facebook_Id_3 = "quan_yingjie@nhn-st.com";
-global.facebook_pW_3 = "quantaihao1-24";
+global.facebook_pW_3 = "xingjunnihao123";
 
 global.facebook_Id_4 = "quan_yingjie@nhn-st.com";
-global.facebook_pW_4 = "quantaihao1-24";
+global.facebook_pW_4 = "xingjunnihao123";
 
 global.facebook_Id_5 = "quan_yingjie@nhn-st.com";
 global.facebook_pW_5 = "quantaihao1-24";
 
 global.facebook_Id_6 = "quan_yingjie@nhn-st.com";
-global.facebook_pW_6 = "quantaihao1-24";
+global.facebook_pW_6 = "xingjunnihao123";
 
 global.facebook_Id_7 = "quan_yingjie@nhn-st.com";
-global.facebook_pW_7 = "quantaihao1-24";
+global.facebook_pW_7 = "xingjunnihao123";
 
 global.facebook_Id_8 = "quan_yingjie@nhn-st.com";
-global.facebook_pW_8 = "quantaihao1-24";
+global.facebook_pW_8 = "xingjunnihao123";
 
 global.facebook_Id_9 = "quan_yingjie@nhn-st.com";
-global.facebook_pW_9 = "quantaihao1-24";
+global.facebook_pW_9 = "xingjunnihao123";
 
 global.facebook_Id_10 = "quan_yingjie@nhn-st.com";
-global.facebook_pW_10 = "quantaihao1-24";
+global.facebook_pW_10 = "xingjunnihao123";
 
 // 발송 메시지 내용
-global.facebook_Msg_1 = "quantaihao1-24";
-global.facebook_Msg_2 = "quantaihao1-24";
-global.facebook_Msg_3 = "quantaihao1-24";
-global.facebook_Msg_4 = "quantaihao1-24";
-global.facebook_Msg_5 = "quantaihao1-24";
-global.facebook_Msg_6 = "quantaihao1-24";
-global.facebook_Msg_7 = "quantaihao1-24";
-global.facebook_Msg_8 = "quantaihao1-24";
-global.facebook_Msg_9 = "quantaihao1-24";
-global.facebook_Msg_10 = "quantaihao1-24";
+global.facebook_Msg_1 = "xingjunnihao123";
+global.facebook_Msg_2 = "xingjunnihao123";
+global.facebook_Msg_3 = "xingjunnihao123";
+global.facebook_Msg_4 = "xingjunnihao123";
+global.facebook_Msg_5 = "xingjunnihao123";
+global.facebook_Msg_6 = "xingjunnihao123";
+global.facebook_Msg_7 = "xingjunnihao123";
+global.facebook_Msg_8 = "xingjunnihao123";
+global.facebook_Msg_9 = "xingjunnihao123";
+global.facebook_Msg_10 = "xingjunnihao123";
 
