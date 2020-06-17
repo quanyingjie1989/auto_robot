c81e76de6a832d2ea55c8a993abd2537aee45080
quanyingjie1989 <65935473+quanyingjie1989@users.noreply.github.com>
2020-06-17 19:14:10 +0900

    update
    update

--- pages/utils/twitter-global.js
+++ pages/utils/twitter-global.js
@@ -17,6 +17,18 @@ global.twitter_pW_5 = "gksehddnr11";
 global.twitter_Id_6 = "jieun8274";
 global.twitter_pW_6 = "dlawldjs0106890";
 
+global.twitter_Id_7 = "ji";
+global.twitter_pW_7 = "dl";
+
+global.twitter_Id_8 = "ji";
+global.twitter_pW_8 = "dl";
+
+global.twitter_Id_9 = "ji";
+global.twitter_pW_9 = "dl";
+
+global.twitter_Id_10 = "ji";
+global.twitter_pW_10 = "dl";
+
 
 // 발송 메시지 내용
 global.twitter_Msg_1 = "원치 않는 임신❤️미국미프진❤️ ♥kakao:QNMB♥ ❤️임신초기낙태❤️갑자기 예상치 못하게 다가오는 임신에 대처방법❤️24시간 관리팀 당신에게 지원합니다";
@@ -25,5 +37,9 @@ global.twitter_Msg_3 = "낙태약구입처 임신초기낙태방법은 낙태알
 global.twitter_Msg_4 = "모든 자연유산이 임신중절수술을 받아야 하나요?♥ 약물유산은 자연유산과 같은 과정입니다.제대로 배출만 되시면 수술을 받지 않아도 됩니다.♥kakao:QNMB♥ 중절수술가능,임신중절수술 혼전임신 임신초기낙태비용 미프진구입방법 인교동";
 global.twitter_Msg_5 = "정품 미프진 ♥ 정품낙태약 정품미프진직구 정품낙태약직구 미프진 (낙태약)이란 먹는 유산약으로서 수술의 고통이 없이 보다 안전하고 조용히 낙태를 할수있는 약입니다 ♥kakao:QNMB♥ 낙태알약 낙태비용 낙태방법  발안리";
 global.twitter_Msg_6 = "옳지 않는 복용방법과 케어가 제품의 심각한 부작용을 초래한다.약물유산 임신중절 방법중 하나이다.♥kakao:QNMB♥ 미국미프진 약물유산 자연유산방법 낙태방법 낙태알약 수원시";
-
+global.twitter_Msg_7 = "test";
+global.twitter_Msg_8 = "test";
+global.twitter_Msg_9 = "test";
+global.twitter_Msg_10 =  "test";
+  
 
