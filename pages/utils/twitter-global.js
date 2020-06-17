9bfb7d1dab45e215add27ef8f45d414905e7b8c9
quanyingjie1989 <65935473+quanyingjie1989@users.noreply.github.com>
2020-06-17 16:25:42 +0800

    Update twitter-global.js

--- pages/utils/twitter-global.js
+++ pages/utils/twitter-global.js
@@ -1,43 +1,29 @@
 // twitter 계정 
-global.twitter_Id_1 = "quantaihao123";
-global.twitter_pW_1 = "test123!";
+global.twitter_Id_1 = "jic8459";
+global.twitter_pW_1 = "wjsdlrcks12";
 
-global.twitter_Id_2 = "dluitest";
-global.twitter_pW_2 = "jpcms123!";
+global.twitter_Id_2 = "jidan2da";
+global.twitter_pW_2 = "wleks1~!";
 
-global.twitter_Id_3 = "dluitest";
-global.twitter_pW_3 = "jpcms123!";
+global.twitter_Id_3 = "jieun004";
+global.twitter_pW_3 = "lje4860213";
 
-global.twitter_Id_4 = "dluitest";
-global.twitter_pW_4 = "jpcms123!";
+global.twitter_Id_4 = "jieun011204";
+global.twitter_pW_4 = "awd59225922";
 
-global.twitter_Id_5 = "dluitest";
-global.twitter_pW_5 = "jpcms123!";
+global.twitter_Id_5 = "jieun4467";
+global.twitter_pW_5 = "gksehddnr11";
 
-global.twitter_Id_6 = "dluitest";
-global.twitter_pW_6 = "jpcms123!";
+global.twitter_Id_6 = "jieun8274";
+global.twitter_pW_6 = "dlawldjs0106890";
 
-global.twitter_Id_7 = "dluitest";
-global.twitter_pW_7 = "jpcms123!";
-
-global.twitter_Id_8 = "dluitest";
-global.twitter_pW_8 = "jpcms123!";
-
-global.twitter_Id_9 = "dluitest";
-global.twitter_pW_9 = "jpcms123!";
-
-global.twitter_Id_10 = "dluitest";
-global.twitter_pW_10 = "jpcms123!";
 
 // 발송 메시지 내용
-global.twitter_Msg_1 = "jpcms123!";
-global.twitter_Msg_2 = "jpcms123!";
-global.twitter_Msg_3 = "jpcms123!";
-global.twitter_Msg_4 = "jpcms123!";
-global.twitter_Msg_5 = "jpcms123!";
-global.twitter_Msg_6 = "jpcms123!";
-global.twitter_Msg_7 = "jpcms123!";
-global.twitter_Msg_8 = "jpcms123!";
-global.twitter_Msg_9 = "jpcms123!";
-global.twitter_Msg_10 = "jpcms123!";
+global.twitter_Msg_1 = "원치 않는 임신❤️미국미프진❤️ ♥kakao:QNMB♥ ❤️임신초기낙태❤️갑자기 예상치 못하게 다가오는 임신에 대처방법❤️24시간 관리팀 당신에게 지원합니다";
+global.twitter_Msg_2 = "미프진도입이슈❤️ ♥kakao:QNMB♥ ❤️유산유도제❤️태아의 문제나 원하지 않는 임신으로 고통 받고 있는 여성들에게 임신초기낙태 방법 지원합니다";
+global.twitter_Msg_3 = "낙태약구입처 임신초기낙태방법은 낙태알약미프진입니다. ♥kakao:QNMB♥ 미프진효과 임신중절약물낙태약은 마취 필요없는 유산방법입니다.♥kakao:QNMB♥임신중절약물낙태방법입니다 낙태방법 유산알약 임신약";
+global.twitter_Msg_4 = "모든 자연유산이 임신중절수술을 받아야 하나요?♥ 약물유산은 자연유산과 같은 과정입니다.제대로 배출만 되시면 수술을 받지 않아도 됩니다.♥kakao:QNMB♥ 중절수술가능,임신중절수술 혼전임신 임신초기낙태비용 미프진구입방법 인교동";
+global.twitter_Msg_5 = "정품 미프진 ♥ 정품낙태약 정품미프진직구 정품낙태약직구 미프진 (낙태약)이란 먹는 유산약으로서 수술의 고통이 없이 보다 안전하고 조용히 낙태를 할수있는 약입니다 ♥kakao:QNMB♥ 낙태알약 낙태비용 낙태방법  발안리";
+global.twitter_Msg_6 = "옳지 않는 복용방법과 케어가 제품의 심각한 부작용을 초래한다.약물유산 임신중절 방법중 하나이다.♥kakao:QNMB♥ 미국미프진 약물유산 자연유산방법 낙태방법 낙태알약 수원시";
+
 
